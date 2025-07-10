fa5fec06721ecc2ca330e3839fb71019e03ce7b4
Andrei <andrei.i@posteo.de>
2025-07-10 00:00:00 +0000

    Open channel from Alice to LSPS2 server in entrypoint.sh

--- lightningd-alice/entrypoint.sh
+++ lightningd-alice/entrypoint.sh
@@ -8,8 +8,10 @@ while read -r i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
     < <(inotifywait -e create,open --format '%f' --quiet "/data/.lightning/regtest" --monitor)
 
 echo "Core-Lightning generating keys and certificates"
-while read -r i; do if [ "$i" = "client-key.pem" ]; then break; fi; done \
-    < <(inotifywait -e create,open --format '%f' --quiet "/data/.lightning/regtest" --monitor)
+if [ ! -f "/data/.lightning/regtest/client-key.pem" ]; then
+    while read -r i; do if [ "$i" = "client-key.pem" ]; then break; fi; done \
+        < <(inotifywait -e create,open --format '%f' --quiet "/data/.lightning/regtest" --monitor)
+fi
 echo "Changing permissions for /data/.lightning/regtest/client-key.pem"
 chmod a+r /data/.lightning/regtest/client-key.pem
 
@@ -36,10 +38,13 @@ do
     sleep 1
 done
 
-lsp_id=$(cat /data/.lightning-lsp/regtest/pubkey)
-lightning-cli --regtest --lightning-dir /data/.lightning connect "$lsp_id" lsp-lightningd
+cln_lsp_id=$(cat /data/.lightning-lsp/regtest/pubkey)
+lightning-cli --regtest --lightning-dir /data/.lightning connect "$cln_lsp_id" lsp-lightningd
 
-channelcount=$(lightning-cli --regtest --lightning-dir /data/.lightning listpeerchannels | jq '.channels | length')
+lsps2_id=$(curl --no-progress-meter lsps2-server:9736/getid)
+lightning-cli --regtest --lightning-dir /data/.lightning connect "$lsps2_id" lsps2-server 9735
+
+channelcount=$(lightning-cli --regtest --lightning-dir /data/.lightning listpeerchannels $cln_lsp_id | jq '.channels | length')
 if [ "$channelcount" -lt 1 ]
 then
     echo "Telling miner to mine 6 blocks"
@@ -57,11 +62,22 @@ then
         sleep 1
     done
 
-    echo "Creating channel to peer"
-    lightning-cli --regtest --lightning-dir /data/.lightning fundchannel id="$lsp_id" amount=100000000sat push_msat=50000000sat
+	echo "Creating channel to peer $cln_lsp_id"
+    lightning-cli --regtest --lightning-dir /data/.lightning fundchannel id="$cln_lsp_id" amount=100000000sat push_msat=50000000sat
 
     echo "Telling miner to mine 7 blocks"
     curl -i -H "Accept: application/json" "miner:8888/mine?blocks=7"
 fi
+
+channelcount=$(lightning-cli --regtest --lightning-dir /data/.lightning listpeerchannels $lsps2_id | jq '.channels | length')
+if [ "$channelcount" -lt 1 ]
+then
+	echo "Creating channel to peer $lsps2_id"
+    lightning-cli --regtest --lightning-dir /data/.lightning fundchannel id="$lsps2_id" amount=100000000sat push_msat=50000000sat
+
+    echo "Telling miner to mine 7 blocks"
+    curl -i -H "Accept: application/json" "miner:8888/mine?blocks=7"
+fi
+
 
 fg %-
